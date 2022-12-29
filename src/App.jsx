0da624bd20906ec4a5e7b913f09c1e56aebe6376
esther921 <estherniyonkuru51@gmail.com>
2022-12-29 16:32:10 +0200

    feat:added a wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Wrapper } from "./components/wrapper/Wrapper";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Wrapper />
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
 }
